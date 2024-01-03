51d9aadd09ef86a83b841ffc467e737992d3efaf
Kevin Grove <bqcritken@gmail.com>
2024-01-03 08:13:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ToDoList from "./ToDoList";
 import GroceryList from "./GroceryList";
 import Settings from "./Settings";
 import About from "./About";
+import Layout from "./Layout";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav className="mainNav">
-          <ul>
-            <li>
-              <Link to="/todolist">To Do</Link>
-            </li>
-            <li>
-              <Link to="/grocery">Grocery</Link>
-            </li>
-            <li>
-              <Link to="/settings">Settings</Link>
-            </li>
-            <li>
-              <Link to="/aboutpage">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route index path="/todolist" element={<ToDoList />} />
-          <Route path="/grocery" element={<GroceryList />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/aboutpage" element={<About />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          index
+          path="/"
+          element={
+            <Layout>
+              <ToDoList />{" "}
+            </Layout>
+          }
+        />
+        <Route
+          path="/grocery"
+          element={
+            <Layout>
+              <GroceryList />{" "}
+            </Layout>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <Layout>
+              <Settings />{" "}
+            </Layout>
+          }
+        />
+        <Route
+          path="/aboutpage"
+          element={
+            <Layout>
+              <About />{" "}
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
