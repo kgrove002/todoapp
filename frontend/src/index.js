84efd13262d3c3a5b90eb1f3b01b91829707494c
Kevin M Grove <mundonolikeyou@gmail.com>
2025-01-09 11:36:32 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router> {/*  basename="/todoapp" add when deploying! */}
+    <Router basename="/todoapp"> {/*  basename="/todoapp" add when deploying! */}
     <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
